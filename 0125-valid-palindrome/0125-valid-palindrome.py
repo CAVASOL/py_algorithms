2b6acd672e5c51c2c0467f9b9384cffdd85fa702
김연 <awyeon@gmail.com>
2023-10-30 19:02:50 +0900

    deleted

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = [t.lower() for t in s if t.isalnum()]
-
-        return all (s[i] == s[~i] for i in range(len(s)//2))
-
-
-            
